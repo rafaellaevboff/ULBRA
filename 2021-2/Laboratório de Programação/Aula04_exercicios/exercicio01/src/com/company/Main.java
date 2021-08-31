371d235dd1b466ebe0cfe746ae34e2b0a1049b4a
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2021-08-31 19:58:11 -0300

    adicionei variáveis do tipo constante

--- 2021-2/Laboratório de Programação/Aula04_exercicios/exercicio01/src/com/company/Main.java
+++ 2021-2/Laboratório de Programação/Aula04_exercicios/exercicio01/src/com/company/Main.java
@@ -5,13 +5,15 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-    int num, soma = 0;
+    int sorteado, soma = 0;
+    final int QTD_SORTEIOS=3;
+    final int FACES_DADO = 6;
     Random r = new Random();
 
-    for( int i = 0; i < 3; i++){
-        num = r.nextInt(6)+1;
-        System.out.println("O número sorteado foi " + num);
-        soma += num;
+    for( int i = 0; i < QTD_SORTEIOS; i++){
+        sorteado = r.nextInt(FACES_DADO)+1;
+        System.out.println("O número sorteado foi " + sorteado);
+        soma += sorteado;
     }
         System.out.println("A soma dos números sorteados é " + soma);
     }
