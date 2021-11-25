42be3a63761e079ccd42d8f423141299bdd070a2
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2021-11-25 17:53:10 -0300

    alterações

--- 2021-2/Engenharia de Software/trabalho_testesJunit/testeCalculadora/src/test/java/tests/SignTest.java
+++ 2021-2/Engenharia de Software/trabalho_testesJunit/testeCalculadora/src/test/java/tests/SignTest.java
@@ -6,31 +6,51 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SignTest {
+    Calculos cal = new Calculos();
+
+    //SOMA
+    @Test
+    public void testReturnSom1(){
+        assertEquals(15, cal.som2(10,5));
+    }
     @Test
     public void testReturnSom2(){
-        Calculos cal = new Calculos();
-        assertEquals(15, cal.som2(10,5));
         assertEquals(15, cal.som2(10,15));
     }
 
+
+    //SUBTRAÇÃO
+    @Test
+    public void testReturnSub1(){
+        assertEquals(20, cal.sub2(22,2));
+
+    }
     @Test
     public void testReturnSub2(){
-        Calculos cal = new Calculos();
-        assertEquals(20, cal.sub2(10,5));
         assertEquals(15, cal.sub2(10,5));
     }
 
+
+    //MULTIPLICAÇÃO
+    @Test
+    public void testReturnMult1(){
+        assertEquals(50, cal.mult2(10,5));
+    }
     @Test
     public void testReturnMult2(){
-        Calculos cal = new Calculos();
-        assertEquals(50, cal.mult2(10,5));
         assertEquals(15, cal.mult2(10,5));
     }
 
+
+    //DIVISÃO
+    @Test
+    public void testReturnDiv1(){
+        Calculos cal = new Calculos();
+        assertEquals(8, cal.div2(16,2));
+    }
     @Test
     public void testReturnDiv2(){
         Calculos cal = new Calculos();
-        assertEquals(8, cal.div2(10,5));
         assertEquals(15, cal.div2(10,5));
     }
 
