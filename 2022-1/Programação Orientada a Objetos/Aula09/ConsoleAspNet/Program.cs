6e49f4fa22a7e74d2985d511bc3de60b47b21378
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-04-25 22:29:30 -0300

    novos arquivos

---
+++ 2022-1/Programação Orientada a Objetos/Aula09/ConsoleAspNet/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+//dotnet add package Microsoft.AspNetCore --version 2.2.0
+
+namespace ConsoleAspNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(
+                    app => {
+                        app.Run(context =>
+                         context.Response.WriteAsync("<h1>Hello World<h1>"));
+                    }
+                )
+  
+    }
+}
