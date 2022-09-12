b4a8b64a0eac278284b4a33ac73672c0f1ade4db
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-09-11 23:13:06 -0300

    novas alterações

--- 2022-2/Tecnologias Moveis/Bootcamp-Kotlin/conceitos/src/main/kotlin/AMS2.kt
+++ 2022-2/Tecnologias Moveis/Bootcamp-Kotlin/conceitos/src/main/kotlin/AMS2.kt
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 fun main(args: Array<String>){
     println(if(args[0].toInt() < 12) "Bom dia Kotlin" else "Boa noite, Kotlin")
-=======
-class AMS2 {
->>>>>>> 00ed0e9edee8c82a3990aa56d9545cf9d5d044f7
-}
